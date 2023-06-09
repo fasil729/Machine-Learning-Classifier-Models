725b430b06453db3879ba99fdab7f9d5cab599e9
Deribew <deribewsoftware@gmail.com>
2023-06-09 03:54:34 -0700

    added

--- Wether Demo/wetherFeatures.py
+++ Wether Demo/wetherFeatures.py
@@ -9,11 +9,19 @@ class DataFeature():
     data= open(weatherdata,'r').readlines()
     for D in data:
       
-      print(len(D.split(',')))
+      print(D.split(',')[:21])
+  def labelOfWeather(self,weatherdata):
+    data= open(weatherdata,'r').readlines()
+    label=[]
+    for D in data:
+      
+      label.append(D.split(',')[21])
+    label.pop(0)
    
- 
+    return label
 dataset=DataFeature()
  
 data='data/weather.csv'
 dataset.get_weatherFeatures(data)
+print(dataset.labelOfWeather(data))
  
